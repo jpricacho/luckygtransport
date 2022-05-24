fdad2e83f62a5e115ac9423a86cf61bafa71ce67
melizadeguia <melizadeguia98@gmail.com>
2022-05-24 21:57:13 +0800

    edited frontpage

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,9 +1,15 @@
-
-import { Row, Container, Col, Form } from 'react-bootstrap';
-
+import { Row, Container, Col, Form, Carousel } from 'react-bootstrap';
 import Promos from './Promos';
 import Reviews from './Reviews';
 import Pricing from './Pricing';
+import ford from '../media/ford.png'
+import honda from '../media/honda.png'
+import hyundai from '../media/hyundai.png'
+import isuzu from '../media/isuzu.png'
+import kia from '../media/kia.png'
+import mitsubishi from '../media/mitsubishi.png'
+import nissan from '../media/nissan.png'
+import toyota from '../media/toyota.png'
 import '../styles/FrontPage.scss'
 
 
@@ -85,16 +91,66 @@ const FrontPage = () => {
 
             </div>
 
-            <Container fluid='sm'>
-                <Row className='brands'>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                </Row>
-            </Container>
+            <Carousel variant='dark' className='brands' controls={false}>
+                <Carousel.Item>
+                    <Container fluid='sm'>
+                        <Row>
+                            <div className='logos'>
+                                <Col sm>
+                                    <div className='image'>
+                                        <img src={ford} />
+                                    </div>
+                                </Col>
+                                <Col sm>
+                                    <div className='image'>
+                                        <img src={honda} />
+                                    </div>
+                                </Col>
+                                <Col sm>
+                                    <div className='image'>
+                                        <img src={hyundai} />
+                                    </div>
+                                </Col>
+                                <Col sm>
+                                    <div className='image'>
+                                        <img src={isuzu} />
+                                    </div>
+                                </Col>
+                            </div>
+                        </Row>
+                    </Container>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <Container fluid='sm'>
+                        <Row>
+                            <div className='logos'>
+                                <Col sm>
+                                    <div className='image'>
+                                        <img src={kia} />
+                                    </div>
+                                </Col>
+
+                                <Col sm>
+                                    <div className='image'>
+                                        <img src={mitsubishi} />
+                                    </div>
+                                </Col>
+                                <Col sm>
+                                    <div className='image'>
+                                        <img src={nissan} />
+                                    </div>
+                                </Col>
+                                <Col sm>
+                                    <div className='image'>
+                                        <img src={toyota} />
+                                    </div>
+                                </Col>
+                            </div>
+                        </Row>
+                    </Container>
+                </Carousel.Item>
+            </Carousel>
 
             <Promos />
             <Reviews />
